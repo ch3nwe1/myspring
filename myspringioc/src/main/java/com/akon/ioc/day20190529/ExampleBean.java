a4a8fbf6cda89cae8052772465bae580969e44e2
ch3nwe1 <ch3nwe1@163.com>
2019-05-30 09:08:41 +0800

    init

---
+++ myspringioc/src/main/java/com/akon/ioc/day20190529/ExampleBean.java
@@ -0,0 +1,21 @@
+package com.akon.ioc.day20190529;
+
+public class ExampleBean {
+
+    private int years;
+
+    private String ultimateAnswer;
+
+    public ExampleBean(int years, String ultimateAnswer) {
+        this.years = years;
+        this.ultimateAnswer = ultimateAnswer;
+    }
+
+    @Override
+    public String toString() {
+        return "ExampleBean{" +
+                "years=" + years +
+                ", ultimateAnswer='" + ultimateAnswer + '\'' +
+                '}';
+    }
+}
