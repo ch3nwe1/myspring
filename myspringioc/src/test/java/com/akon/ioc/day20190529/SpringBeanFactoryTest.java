60d4590262b69cd0cd3256b96dd3baa309fa45cd
ch3nwe1 <ch3nwe1@163.com>
2019-05-30 09:08:21 +0800

    init

--- myspringioc/src/test/java/com/akon/ioc/day20190529/SpringBeanFactoryTest.java
+++ myspringioc/src/test/java/com/akon/ioc/day20190529/SpringBeanFactoryTest.java
@@ -1,5 +1,7 @@
 package com.akon.ioc.day20190529;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -8,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,4 +25,27 @@ public class SpringBeanFactoryTest {
         logger.info(clientService.getClass().getCanonicalName());
     }
 
+    @Test
+    public void test02(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("day20190529/example02.xml");
+        ExampleBean exampleBean = context.getBean(ExampleBean.class);
+        Assert.assertNotNull(exampleBean);
+        logger.info(exampleBean.toString());
+    }
+
+    @Test
+    public void testBeanByIndex(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("day20190529/example03.xml");
+        ExampleBean exampleBean = context.getBean(ExampleBean.class);
+        Assert.assertNotNull(exampleBean);
+        logger.info(exampleBean.toString());
+    }
+
+    @Test
+    public void testDataSource(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("day20190529/example04.xml");
+        BasicDataSource dataSource = context.getBean(BasicDataSource.class);
+        Assert.assertNotNull(dataSource);
+        logger.info(dataSource.getUrl());
+    }
 }
