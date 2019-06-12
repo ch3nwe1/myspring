2d3af611040bdb895fa721dc0a21f23b7817b602
ch3nwe1 <ch3nwe1@163.com>
2019-06-12 13:59:42 +0800

    init

---
+++ designpartterns/src/main/java/com/akon/adapter/Duck.java
@@ -0,0 +1,9 @@
+package com.akon.adapter;
+
+public interface Duck {
+
+
+    void quack();
+    void fly();
+
+}
---
+++ designpartterns/src/main/java/com/akon/adapter/Turkey.java
@@ -0,0 +1,15 @@
+package com.akon.adapter;
+
+public interface Turkey {
+
+    /**
+     * 火鸡咕咕叫
+     */
+    void gobble();
+
+    /**
+     * 火鸡飞行一段距离
+     */
+    void fly();
+
+}
