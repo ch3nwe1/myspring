c4e0f52c30669474629f5d24f3b5233504895953
ch3nwe1 <ch3nwe1@163.com>
2019-06-19 20:58:21 +0800

    状态模式

---
+++ designpartterns/src/main/java/com/akon/state/after/GumballMachine.java
@@ -0,0 +1,67 @@
+package com.akon.state.after;
+
+public class GumballMachine {
+
+    private State soldOutState;
+    private State noQuarterState;
+    private State hasQuarterState;
+    private State soldState;
+
+    private State state = soldState;
+    private int count;
+
+    public GumballMachine(int numberGumballs) {
+        soldOutState = new SoldOutState(this);
+        noQuarterState = new NoQuarterState(this);
+        hasQuarterState = new HasQuarterState(this);
+        soldState = new SoldState(this);
+        this.count = numberGumballs;
+        if(count > 0){
+            state = noQuarterState;
+        }
+    }
+
+    public void insertQuarter(){
+        state.insertQuarter();
+    }
+
+    public void ejectQuarter(){
+        state.ejectQuarter();
+    }
+
+    public void turnCrank(){
+        state.turnCrank();
+        state.dispense();
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public void releaseBall(){
+        System.out.println("A gumball comes rolling out the slot");
+        if(count != 0){
+            count = count - 1;
+        }
+    }
+
+    public State getSoldOutState() {
+        return soldOutState;
+    }
+
+    public State getNoQuarterState() {
+        return noQuarterState;
+    }
+
+    public State getHasQuarterState() {
+        return hasQuarterState;
+    }
+
+    public State getSoldState() {
+        return soldState;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
---
+++ designpartterns/src/main/java/com/akon/state/after/State.java
@@ -0,0 +1,10 @@
+package com.akon.state.after;
+
+public interface State {
+
+    void insertQuarter();
+    void ejectQuarter();
+    void turnCrank();
+    void dispense();
+
+}
